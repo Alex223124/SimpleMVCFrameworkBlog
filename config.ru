1894d21f54670b764d5c19ef2a763286765f91a3
Alex Sologoub <dadeveloper@hotmail.com>
2016-02-23 21:00:59 +0000

    Initial Commit

---
+++ config.ru
@@ -0,0 +1,4 @@
+require './config/application.rb'
+run Blog::Application.new 
+# запускает наше приложение - через команду rack
+
---
+++ config/application.rb
@@ -0,0 +1,7 @@
+require 'simplemvc'
+
+module Blog 
+  class Application < Simplemvc::Application # инхеританс от метода зпуска сервера
+  end
+end 
+
